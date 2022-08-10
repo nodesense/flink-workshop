fe18f47f697357a18aa20dc951e5a70dc56b4f6f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-08-10 21:03:00 +0530

    Update PageView.java

--- src/main/java/workshop/models/PageView.java
+++ src/main/java/workshop/models/PageView.java
@@ -3,6 +3,7 @@ package workshop.models;
 // {"viewtime":1660137705976,"userid":"User_8","pageid":"Page_52"}
 
 public class PageView {
+    public java.sql.Timestamp event_time; // from kafka meta data
 public Long viewtime;
 public  String userid;
 public String pageid;
@@ -10,6 +11,7 @@ public String pageid;
     @Override
     public String toString() {
         return "PageView{" +
+                "event_time=" + event_time +
                 "viewtime=" + viewtime +
                 ", userid='" + userid + '\'' +
                 ", pageid='" + pageid + '\'' +
